11e0b4131bc1fff42c7567512066c19771b5e070
Obfuscoder <obfuscoder@obfusco.de>
2020-11-21 15:02:31 +0100

    Add DRYAD table

--- Mods/tech/mdc/kneeboard/06.lua
+++ Mods/tech/mdc/kneeboard/06.lua
@@ -13,3 +13,18 @@ for i = 1, string.len(mdc.ramrod) do
 	head(1, i-1, i-1)
 	cell(2, i-1, string.sub(mdc.ramrod, i, i))
 end
+
+headline(3, "DRYAD")
+
+columns = 11
+head(4, 0, "")
+for i, header in ipairs(mdc.dryad.header) do
+	head(4, i, i - 1)
+end
+
+for i, row in ipairs(mdc.dryad.rows) do
+	head(4 + i, 0, row.header)
+	for j, col in ipairs(row.columns) do
+		cell(4 + i, j, col)
+	end
+end
